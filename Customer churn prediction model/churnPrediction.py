8cb65f39f906041c62901592c842bc004c058539
Büşra Cin <69642923+busraCin@users.noreply.github.com>
2023-06-26 23:52:03 +0300

    churn prediction model

---
+++ Customer churn prediction model/churnPrediction.py
@@ -0,0 +1,176 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from catboost import CatBoostClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score,roc_auc_score
+from sklearn.model_selection import train_test_split
+
+
+import warnings
+warnings.simplefilter(action="ignore")
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 170)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+df = pd.read_csv("Telco-Customer-Churn.csv")
+df.head()
+df.shape
+df.info()
+
+
+df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors='coerce')
+df["Churn"] = df["Churn"].apply(lambda x : 1 if x == "Yes" else 0)
+
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+check_df(df)
+
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and dataframe[col].dtypes == "O"]
+
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+
+    return cat_cols, num_cols, cat_but_car
+
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+cat_cols
+num_cols
+cat_but_car
+
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("--------------------------------------------")
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show()
+
+for col in cat_cols:
+    cat_summary(df, col)
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show()
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
+
+
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
+
+for col in num_cols:
+    target_summary_with_num(df, "Churn", col)
+
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(categorical_col)
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean(),
+                        "Count": dataframe[categorical_col].value_counts(),
+                        "Ratio": 100 * dataframe[categorical_col].value_counts() / len(dataframe)}), end="\n\n\n")
+
+for col in cat_cols:
+    target_summary_with_cat(df, "Churn", col)
+
+
+
+
+df[num_cols].corr()
+
+
+f, ax = plt.subplots(figsize=[18, 13])
+sns.heatmap(df[num_cols].corr(), annot=True, fmt=".2f", ax=ax, cmap="magma")
+ax.set_title("Correlation Matrix", fontsize=20)
+plt.show()
+
+
+
+df.corrwith(df["Churn"]).sort_values(ascending=False)
+
+
+df.isnull().sum()
+
+def missing_values_table(dataframe, na_name=False):
+    na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+    n_miss = dataframe[na_columns].isnull().sum().sort_values(ascending=False)
+    ratio = (dataframe[na_columns].isnull().sum() / dataframe.shape[0] * 100).sort_values(ascending=False)
+    missing_df = pd.concat([n_miss, np.round(ratio, 2)], axis=1, keys=['n_miss', 'ratio'])
+    print(missing_df, end="\n")
+    if na_name:
+        return na_columns
+
+na_columns = missing_values_table(df, na_name=True)
+
+df["TotalCharges"].fillna(df["TotalCharges"].median(), inplace=True)  # aylık ödenecek miktarlarıyla totalcharge doldurulailir (daya iyi olur denensin)  veya 11 değişken drop edilebilir
+
+df.isnull().sum()
+
+
+dff = df.copy()
+cat_cols = [col for col in cat_cols if col not in ["Churn"]]
+cat_cols
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+dff = one_hot_encoder(dff, cat_cols, drop_first=True)
+
+
+y = dff["Churn"]
+X = dff.drop(["Churn","customerID"], axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=17)
+
+catboost_model = CatBoostClassifier(verbose=False, random_state=12345).fit(X_train, y_train)
+y_pred = catboost_model.predict(X_test)
+
+
+print(f"Accuracy: {round(accuracy_score(y_pred, y_test), 4)}")
+print(f"Recall: {round(recall_score(y_pred,y_test),4)}")
+print(f"Precision: {round(precision_score(y_pred,y_test), 4)}")
+print(f"F1: {round(f1_score(y_pred,y_test), 4)}")
+print(f"Auc: {round(roc_auc_score(y_pred,y_test), 4)}")
+
+# Accuracy: 0.7837
+# Recall: 0.6333
+# Precision: 0.4843
+# F1: 0.5489
+# Auc: 0.7282
