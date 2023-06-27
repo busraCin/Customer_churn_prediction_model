0b90051d2561c73cc57127b7402c6b6d323b6036
Büşra Cin <69642923+busraCin@users.noreply.github.com>
2023-06-27 23:10:30 +0300

    model development

--- Customer churn prediction model/churnPrediction.py
+++ Customer churn prediction model/churnPrediction.py
@@ -1,3 +1,4 @@
+#Required Library and Functions
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,25 +6,28 @@ import seaborn as sns
 from catboost import CatBoostClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score,roc_auc_score
 from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import LabelEncoder
 
 import warnings
 warnings.simplefilter(action="ignore")
 
+#Visual settings
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 170)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
+#Data Reading
 df = pd.read_csv("Telco-Customer-Churn.csv")
 df.head()
 df.shape
 df.info()
 
-
+# TotalCharges must be a numeric variable
 df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors='coerce')
 df["Churn"] = df["Churn"].apply(lambda x : 1 if x == "Yes" else 0)
 
+#Overview of the data
 def check_df(dataframe, head=5):
     print("##################### Shape #####################")
     print(dataframe.shape)
@@ -40,6 +44,7 @@ def check_df(dataframe, head=5):
 
 check_df(df)
 
+# CAPTURE OF NUMERICAL AND CATEGORY VARIABLES
 
 def grab_col_names(dataframe, cat_th=10, car_th=20):
     cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
@@ -61,14 +66,9 @@ def grab_col_names(dataframe, cat_th=10, car_th=20):
 
     return cat_cols, num_cols, cat_but_car
 
-
 cat_cols, num_cols, cat_but_car = grab_col_names(df)
 
-cat_cols
-num_cols
-cat_but_car
-
-
+# ANALYSIS OF CATEGORY VARIABLES
 def cat_summary(dataframe, col_name, plot=False):
     print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
                         "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
@@ -80,6 +80,7 @@ def cat_summary(dataframe, col_name, plot=False):
 for col in cat_cols:
     cat_summary(df, col)
 
+# ANALYSIS OF NUMERICAL VARIABLES
 def num_summary(dataframe, numerical_col, plot=False):
     quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
     print(dataframe[numerical_col].describe(quantiles).T)
@@ -93,41 +94,32 @@ def num_summary(dataframe, numerical_col, plot=False):
 for col in num_cols:
     num_summary(df, col, plot=True)
 
-
+#target
 def target_summary_with_num(dataframe, target, numerical_col):
     print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
-
 for col in num_cols:
     target_summary_with_num(df, "Churn", col)
-
-
 def target_summary_with_cat(dataframe, target, categorical_col):
     print(categorical_col)
     print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean(),
                         "Count": dataframe[categorical_col].value_counts(),
                         "Ratio": 100 * dataframe[categorical_col].value_counts() / len(dataframe)}), end="\n\n\n")
-
 for col in cat_cols:
     target_summary_with_cat(df, "Churn", col)
 
 
-
-
+# CORRELATION
 df[num_cols].corr()
-
-
 f, ax = plt.subplots(figsize=[18, 13])
 sns.heatmap(df[num_cols].corr(), annot=True, fmt=".2f", ax=ax, cmap="magma")
 ax.set_title("Correlation Matrix", fontsize=20)
 plt.show()
 
-
-
 df.corrwith(df["Churn"]).sort_values(ascending=False)
 
 
+#MISSING VALUE ANALYSIS
 df.isnull().sum()
-
 def missing_values_table(dataframe, na_name=False):
     na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
     n_miss = dataframe[na_columns].isnull().sum().sort_values(ascending=False)
@@ -140,10 +132,9 @@ def missing_values_table(dataframe, na_name=False):
 na_columns = missing_values_table(df, na_name=True)
 
 df["TotalCharges"].fillna(df["TotalCharges"].median(), inplace=True)  # aylık ödenecek miktarlarıyla totalcharge doldurulailir (daya iyi olur denensin)  veya 11 değişken drop edilebilir
-
 df.isnull().sum()
 
-
+#BASE MODEL
 dff = df.copy()
 cat_cols = [col for col in cat_cols if col not in ["Churn"]]
 cat_cols
@@ -162,7 +153,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random
 catboost_model = CatBoostClassifier(verbose=False, random_state=12345).fit(X_train, y_train)
 y_pred = catboost_model.predict(X_test)
 
-
 print(f"Accuracy: {round(accuracy_score(y_pred, y_test), 4)}")
 print(f"Recall: {round(recall_score(y_pred,y_test),4)}")
 print(f"Precision: {round(precision_score(y_pred,y_test), 4)}")
@@ -174,3 +164,124 @@ print(f"Auc: {round(roc_auc_score(y_pred,y_test), 4)}")
 # Precision: 0.4843
 # F1: 0.5489
 # Auc: 0.7282
+
+# Outlier Analysis
+def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+def replace_with_thresholds(dataframe, variable, q1=0.05, q3=0.95):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable, q1=0.05, q3=0.95)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+# Outlier Analysis and Suppression Process
+for col in num_cols:
+    print(col, check_outlier(df, col))
+    if check_outlier(df, col):
+        replace_with_thresholds(df, col)
+
+
+#FEATURE INFERENCE
+
+# Generating annual categorical variable from Tenure variable
+df.loc[(df["tenure"]>=0) & (df["tenure"]<=12),"NEW_TENURE_YEAR"] = "0-1 Year"
+df.loc[(df["tenure"]>12) & (df["tenure"]<=24),"NEW_TENURE_YEAR"] = "1-2 Year"
+df.loc[(df["tenure"]>24) & (df["tenure"]<=36),"NEW_TENURE_YEAR"] = "2-3 Year"
+df.loc[(df["tenure"]>36) & (df["tenure"]<=48),"NEW_TENURE_YEAR"] = "3-4 Year"
+df.loc[(df["tenure"]>48) & (df["tenure"]<=60),"NEW_TENURE_YEAR"] = "4-5 Year"
+df.loc[(df["tenure"]>60) & (df["tenure"]<=72),"NEW_TENURE_YEAR"] = "5-6 Year"
+
+# Specify contract 1 or 2 year customers as Engaged
+df["NEW_Engaged"] = df["Contract"].apply(lambda x: 1 if x in ["One year","Two year"] else 0)
+
+# People who do not receive any support, backup or protection
+df["NEW_noProt"] = df.apply(lambda x: 1 if (x["OnlineBackup"] != "Yes") or (x["DeviceProtection"] != "Yes") or (x["TechSupport"] != "Yes") else 0, axis=1)
+
+# Young customers with monthly contracts
+df["NEW_Young_Not_Engaged"] = df.apply(lambda x: 1 if (x["NEW_Engaged"] == 0) and (x["SeniorCitizen"] == 0) else 0, axis=1)
+
+# The total number of services received by the person
+df['NEW_TotalServices'] = (df[['PhoneService', 'InternetService', 'OnlineSecurity',
+                                       'OnlineBackup', 'DeviceProtection', 'TechSupport',
+                                       'StreamingTV', 'StreamingMovies']]== 'Yes').sum(axis=1)
+
+# People who buy any streaming service
+df["NEW_FLAG_ANY_STREAMING"] = df.apply(lambda x: 1 if (x["StreamingTV"] == "Yes") or (x["StreamingMovies"] == "Yes") else 0, axis=1)
+
+# Kişi otomatik ödeme yapıyor mu?
+df["NEW_FLAG_AutoPayment"] = df["PaymentMethod"].apply(lambda x: 1 if x in ["Bank transfer (automatic)","Credit card (automatic)"] else 0)
+
+# Average monthly payment
+df["NEW_AVG_Charges"] = df["TotalCharges"] / (df["tenure"] + 1)
+
+# Current Price increase relative to average price
+df["NEW_Increase"] = df["NEW_AVG_Charges"] / df["MonthlyCharges"]
+
+# Fee per service
+df["NEW_AVG_Service_Fee"] = df["MonthlyCharges"] / (df['NEW_TotalServices'] + 1)
+
+
+df.head()
+df.shape
+
+# ENCODING
+# The process of separating variables according to their types
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+# Label Encoding
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+
+binary_cols = [col for col in df.columns if df[col].dtypes == "O" and df[col].nunique() == 2]
+binary_cols
+
+for col in binary_cols:
+    df = label_encoder(df, col)
+
+# One-Hot Encoding
+cat_cols = [col for col in cat_cols if col not in binary_cols and col not in ["Churn", "NEW_TotalServices"]] # cat_cols update
+cat_cols
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+df = one_hot_encoder(df, cat_cols, drop_first=True)
+
+df.head()
+
+# #Creating a model
+y = df["Churn"]
+X = df.drop(["Churn","customerID"], axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=17)
+
+catboost_model = CatBoostClassifier(verbose=False, random_state=12345).fit(X_train, y_train)
+y_pred = catboost_model.predict(X_test)
+
+print(f"Accuracy: {round(accuracy_score(y_pred, y_test), 2)}")
+print(f"Recall: {round(recall_score(y_pred,y_test),2)}")
+print(f"Precision: {round(precision_score(y_pred,y_test), 2)}")
+print(f"F1: {round(f1_score(y_pred,y_test), 2)}")
+print(f"Auc: {round(roc_auc_score(y_pred,y_test), 2)}")
+
+# Accuracy: 0.8
+# Recall: 0.66
+# Precision: 0.51
+# F1: 0.58
+# Auc: 0.75
